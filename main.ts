d402fbdb0d50e91c64284b79e5d370e8e8bf85a2
Vladimir Matveev <vladima@microsoft.com>
2016-03-10 12:19:01 -0800

    initial commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+import {Subject, BehaviorSubject} from "rxjs/rx";
+
+class Task {
+  id:number;
+  title:string;
+  description:string;
+
+  constructor(data:any = {}) {
+    this.id = data.id;
+    this.title = data.title;
+    this.description = data.description;
+  }
+}
+
+let s1: Subject<Task[]>;
+let s2 = new BehaviorSubject([]);
+let arr = [ s1, s2 ]
